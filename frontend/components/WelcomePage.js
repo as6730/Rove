6575fda69517717c5b03332a9354087fde21bea9
Todd <toddleebergman@gmail.com>
2018-04-23 18:42:17 -0700

    Created skeleton for welcome page. Will revisit at later date

---
+++ frontend/components/WelcomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  Keyboard,
+  DatePickerIOS,
+} from 'react-native';
+
+
+class WelcomePage extends React.Component {
+  render() {
+    let pic = {
+      uri: `app/assets/images/sf_city.jpg`,
+    };
+    return (
+      <View>
+        <Image/>
+      </View>
+    );
+  }
+}
