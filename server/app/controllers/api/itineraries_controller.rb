030a56c5752686cfa57f5b439a6132468afb2175
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-24 18:04:51 -0700

    Add time slots to itinerary

--- server/app/controllers/api/itineraries_controller.rb
+++ server/app/controllers/api/itineraries_controller.rb
@@ -8,23 +8,23 @@ class Api::ItinerariesController < ApplicationController
     # get restaurants
     restaurants_hash = {}
     if params[:restaurants].downcase == "true"
-      restaurants_hash["breakfast"] = PlacesUtils.get_places(date, params[:amt].to_i, params[:lat], params[:lon], "restaurant", "breakfast", ["id", "name", "formatted_phone_number", "formatted_address", "rating", "website", "opening_hours", "price_level", "geometry"])
-      restaurants_hash["lunch"] = PlacesUtils.get_places(date, params[:amt].to_i, params[:lat], params[:lon], "restaurant", "lunch", ["id", "name", "formatted_phone_number", "formatted_address", "rating", "website", "opening_hours", "price_level", "geometry"])
-      restaurants_hash["dinner"] = PlacesUtils.get_places(date, params[:amt].to_i, params[:lat], params[:lon], "restaurant", "dinner", ["id", "name", "formatted_phone_number", "formatted_address", "rating", "website", "opening_hours", "price_level", "geometry"])
+      restaurants_hash["breakfast"] = PlacesUtils.get_places(params[:amt].to_i, params[:lat], params[:lon], "restaurant", ["id", "name", "formatted_phone_number", "formatted_address", "rating", "website", "opening_hours", "price_level", "geometry"], "breakfast", params[:date])
+      restaurants_hash["lunch"] = PlacesUtils.get_places(params[:amt].to_i, params[:lat], params[:lon], "restaurant", ["id", "name", "formatted_phone_number", "formatted_address", "rating", "website", "opening_hours", "price_level", "geometry"], "lunch", params[:date])
+      restaurants_hash["dinner"] = PlacesUtils.get_places(params[:amt].to_i, params[:lat], params[:lon], "restaurant", ["id", "name", "formatted_phone_number", "formatted_address", "rating", "website", "opening_hours", "price_level", "geometry"], "dinner", params[:date])
     end
     itinerary << restaurants_hash
 
     # get bars
     bars_hash = {}
     if params[:bars].downcase == "true"
-      bars["bars"] = PlacesUtils.get_places(date, params[:amt].to_i, params[:lat], params[:lon], "bar", ["id", "name", "formatted_phone_number", "formatted_address", "rating", "price_level", "website", "opening_hours", "geometry"])
+      bars_hash["bars"] = PlacesUtils.get_places(params[:amt].to_i, params[:lat], params[:lon], "bar", ["id", "name", "formatted_phone_number", "formatted_address", "rating", "price_level", "website", "opening_hours", "geometry"], params[:date])
     end
     itinerary << bars_hash
 
     # get nature
     nature_hash = {}
     if params[:nature].downcase == "true"
-      nature_hash["nature"] = PlacesUtils.get_places(date, params[:amt].to_i, params[:lat], params[:lon], "park", ["id", "name", "formatted_address", "website", "geometry"])
+      nature_hash["nature"] = PlacesUtils.get_places(params[:amt].to_i, params[:lat], params[:lon], "park", ["id", "name", "formatted_address", "website", "geometry"], params[:date])
     end
     itinerary << nature_hash
 
@@ -32,17 +32,17 @@ class Api::ItinerariesController < ApplicationController
     # TODO: get radius from request
     arts_hash = {}
     if params[:arts].downcase == "true"
-      arts_hash["arts"] = PlacesUtils.get_places(date, params[:amt].to_i, params[:lat], params[:lon], "museum", ["id", "name", "formatted_phone_number", "formatted_address", "website", "geometry", "opening_hours", "website"], 10000)
+      arts_hash["arts"] = PlacesUtils.get_places(params[:amt].to_i, params[:lat], params[:lon], "museum", ["id", "name", "formatted_phone_number", "formatted_address", "website", "geometry", "opening_hours", "website"], params[:date], 10000)
     end
     itinerary << arts_hash
 
     render json: itinerary
   end
-
+  
   private
 
   def itinerary_params
-    defaults = { amt: "2", radius: "2000" }
+    defaults = { amt: "2", radius: "1000", date: "2018" }
     params.permit(:lat, :lon, :bars, :restaurants, :nature, :arts, :amt, :radius, :date).reverse_merge(defaults)
   end
 end
