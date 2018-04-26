e18a95154e30e36fd4ee73ca980b327ae209a565
Rose <rkoron007@gmail.com>
2018-04-26 14:32:49 -0700

    Finished Spinner

--- client/frontend/components/common/Spinner.js
+++ client/frontend/components/common/Spinner.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { View, ActivityIndicator, Text } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 
-
 const OurSpinner = ({ size,text }) => {
   return (
     <View style={styles.spinnerStyle}>
       <Spinner
         visible={true}
         color='#FE5D26'
+        textContent={"Building Your Itinerary..."}
+        textStyle={{color: '#FE5D26', fontFamily: 'Helvetica Neue'}}
         overlayColor="#dddddd"/>
     </View>
   );
@@ -21,11 +22,9 @@ const styles = {
     alignItems: 'center'
   },textStyle:{
     color: '#FE5D26',
-
     fontSize: 20,
     marginBottom: 100,
   }
 };
 
-      // <ActivityIndicator color='#FE5D26'size={size || 'large'} />
 export { OurSpinner };
