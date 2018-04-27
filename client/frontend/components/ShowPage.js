dc8d50630254ac8b018298e6633960fcae324fea
Todd <toddleebergman@gmail.com>
2018-04-27 15:36:07 -0700

    changes to show page

--- client/frontend/components/ShowPage.js
+++ client/frontend/components/ShowPage.js
@@ -90,7 +90,7 @@ class ShowPage extends React.Component {
       latitude: 37.773972,
       longitude: -122.431297,
     };
-    console.log(this.props.place.photo_url);
+    console.log(this.props.place);
 
     return (
       <View style = {styles.container}>
@@ -104,11 +104,10 @@ class ShowPage extends React.Component {
           />
         <View style = {styles.info}>
           <View style = {styles.contactInfo}>
-            <Text style = {styles.title} >Good Food</Text>
-            <Text>2343 Battery St.</Text>
-            <Text>San Francisco, CA 98437</Text>
-            <Text>(555)-555-5555</Text>
-            <Text>Visit Website</Text>
+            <Text style = {styles.title} >{this.props.place.name}</Text>
+            <Text>{this.props.place.formatted_address}</Text>
+            <Text>{this.props.place.formatted_phone_number}</Text>
+            <Text>{this.props.place.website}</Text>
           </View>
         </View>
         <View style = {styles.mapContainer}>
@@ -117,10 +116,10 @@ class ShowPage extends React.Component {
             provider={'google'}
             style = {styles.map}
             initialRegion={{
-              latitude: 37.773972,
-              longitude: -122.431297,
-              latitudeDelta: 0.0092,
-              longitudeDelta: 0.0421,
+              latitude: this.props.place.location.lat,
+              longitude: this.props.place.location.lng,
+              latitudeDelta: 0.0020,
+              longitudeDelta: 0.0100,
             }}/>
         </View>
         <Button onPress={()=> this.submit()} children={"Add to Calendar"}></Button>
@@ -143,7 +142,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-between',
     padding: 25,
-    height: '50%',
+    height: '60%',
     borderBottomWidth: 1,
     borderTopWidth: 1,
     borderColor: '#FE5D26',
@@ -153,7 +152,7 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   mapContainer: {
-    height: '50%',
+    height: '40%',
   },
   map: {
     height: '100%',
